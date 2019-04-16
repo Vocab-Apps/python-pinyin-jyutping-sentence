a985095d07e6ccc43c56270370b217b5441a0a8c
Luc Wastiaux <luc.wastiaux@xsmail.com>
2019-04-17 07:24:53 +0800

    added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pinyin_jyutping_sentence',
+      version='0.1',
+      description='Convert a Chinese sentence to Pinyin or Jyutping',
+      url='https://github.com/lucwastiaux/python-pinyin-jyutping-sentence',
+      author='Luc Wastiaux',
+      author_email='luc.wastiaux@airpost.net',
+      license='GPL',
+      packages=['pinyin_jyutping_sentence'],
+      zip_safe=False)
