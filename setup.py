6a20aa52fea17947133d7f1c5ccf6e544a536d4b
luc <luc.wastiaux@xsmail.com>
2022-07-20 14:55:40 +0800

    bump up version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 # twine upload dist/*
 
 setup(name='pinyin_jyutping_sentence',
-      version='1.2',
+      version='1.3',
       description='Convert a Chinese sentence to Pinyin or Jyutping',
       long_description=open('README.rst', encoding='utf-8').read(),
       url='https://github.com/lucwastiaux/python-pinyin-jyutping-sentence',
